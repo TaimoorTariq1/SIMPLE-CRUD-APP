94bf7e4690921dbbf43337f05e1738d3ed6504b9
Taimoor Codes <132977742+TaimoorTariq1@users.noreply.github.com>
2024-05-06 17:22:56 +0500

    .

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const mongoose = require('mongoose');
+const Product = require("./models/product.model.js");
 const app = express()
+
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -10,9 +13,14 @@ app.get('/', (req, res) => {
     res.send("Hello from Node API Server Updated")
 });
 
-app.post('/api/products', (req, res) => {
-    console.log(req.body);
-    res.send(req.body);
+app.post('/api/products', async (req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product)
+
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 });
 
 mongoose.connect("mongodb+srv://taimoortariq2015:timston5678>@backenddb.lmfkans.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
