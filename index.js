67e69b434b726eeb538f06723cbb293c3c2386de
Taimoor Codes <132977742+TaimoorTariq1@users.noreply.github.com>
2024-05-06 16:19:18 +0500

    .

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
 app.listen(3000, () => {
@@ -7,4 +8,13 @@ app.listen(3000, () => {
 
 app.get('/', (req, res) => {
     res.send("Hello from Node API Server Updated")
+});
+
+mongoose.connect("mongodb+srv://taimoortariq2015:timston5678>@backenddb.lmfkans.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
+.then(() => {
+    console.log("Connected to database");
+    
+})
+.catch(() => {
+    console.log("Connection failed!");
 });
