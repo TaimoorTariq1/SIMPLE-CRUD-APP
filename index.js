01b361f988fca073c21009252d64a2384ba360f7
Taimoor Codes <132977742+TaimoorTariq1@users.noreply.github.com>
2024-05-07 19:51:43 +0500

    .

--- index.js
+++ index.js
@@ -13,7 +13,33 @@ app.get('/', (req, res) => {
     res.send("Hello from Node API Server Updated")
 });
 
-app.post('/api/products', async (req, res) => {
+app.get('/api/products', async (req, res) => {
+
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        
+    }
+});
+
+app.get('/api/products', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Product.findbyID(id);
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        res.status(200).json(product);
+        
+    }
+})
+
+
+app.post('/api/products/:id', async (req, res) => {
     try {
         const product = await Product.create(req.body);
         res.status(200).json(product)
